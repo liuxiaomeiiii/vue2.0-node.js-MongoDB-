b820c51d7bc28126d085b0b479c51b5f6ac04b52
July <July.liu@charleskeith.com>
2019-02-18 17:33:23 +0800

    vue2.0+node.js+MongoDB打造全栈商城

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  html,body,#app{
+    height: 100%;
+  }
+</style>
---
+++ src/views/GoodsList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="goods-list">
+    <nav-header></nav-header>
+    <nav-footer></nav-footer>
+  </div>
+</template>
+
+<script>
+import NavHeader from './../components/NavHeader'
+import NavFooter from './../components/NavFooter'
+export default {
+  components: {
+    NavHeader,
+    NavFooter
+  }
+}
+</script>
+
+<style>
+
+</style>
